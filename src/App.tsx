f4ccb251b52830eafe3640c55b4fb78812818f4d
Pedro Sousa <im.pete.sousa@gmail.com>
2020-11-02 13:20:23 -0300

    Implement GlobaStyles and reset CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import GlobalStyles from './styles/global';
+
 const App: React.FC = () => {
   return (
-    <div className="App">
+    <>
       <h1>Hello</h1>
-    </div>
+      <GlobalStyles />
+    </>
   );
 };
 
