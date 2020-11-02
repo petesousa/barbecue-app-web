0f225abcb73a250f2c8455e081641be4c1b4deb8
Pedro Sousa <im.pete.sousa@gmail.com>
2020-11-02 15:19:01 -0300

    Add SignIn component to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import GlobalStyles from './styles/global';
+import SignIn from './pages/SignIn';
 
 const App: React.FC = () => {
   return (
     <>
-      <h1>Hello</h1>
+      <SignIn />
       <GlobalStyles />
     </>
   );
