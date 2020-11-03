237e415304196008b79899cabaf703019ec5b25a
Pedro Sousa <im.pete.sousa@gmail.com>
2020-11-03 09:45:25 -0300

    Add logout button to home and display toast message when logging out

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { useAuth } from '../../hooks/auth';
+
+import { useToast } from '../../hooks/toast';
 
 import { Container } from './styles';
 
 const Home: React.FC = () => {
   const { signOut } = useAuth();
+  const { addToast } = useToast();
+
+  const handleLogout = useCallback(() => {
+    addToast({
+      type: 'success',
+      title: 'Logout realizado com sucesso!',
+      description: 'Volte sempre :)',
+    });
+    signOut();
+  }, [addToast, signOut]);
+
   return (
     <Container>
-      <button type="button" onClick={() => signOut()}>
+      <button type="button" onClick={handleLogout}>
         Logout
       </button>
     </Container>
