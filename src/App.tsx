b9ef736c00c7a8bd46cfe484ff5b4bf5942bd35f
Pedro Sousa <im.pete.sousa@gmail.com>
2020-11-03 18:40:14 -0300

    Removes unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 import GlobalStyles from './styles/global';
-import SignIn from './pages/SignIn';
 
 import Routes from './routes/index';
 
